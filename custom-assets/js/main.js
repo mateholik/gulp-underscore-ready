3af0d483fa70de40345bacc0efd81b33eca64799
user <user@users-MacBook-Pro.local>
2020-08-17 18:52:10 +0300

    init. Gulp sass, minification, functions.php READY

---
+++ custom-assets/js/main.js
@@ -0,0 +1,9 @@
+// var burger = document.getElementById("burger");
+// var mobNav = document.getElementById("mob-nav");
+// burger.addEventListener("click", function () {
+//   console.log("paspaude");
+//   this.classList.toggle("is-active");
+//   mobNav.classList.toggle("mob-nav--active");
+// });
+
+console.log("zdarof2");
